31bd176eeb86b7d9a30d2259baf85bd6023e076c
Vismaya Wilson <vismayawilson3@gmail.com>
2022-01-20 12:42:56 +0530

    New files

---
+++ shopping-page/src/ShoppingPage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+const Shoppingpage = () => {
+    return (
+        <div>
+            <header>
+           
+            </header>
+            <main>
+                {/* banner message */}
+                <section className="banner">
+                    <h1 className="shopStyle">Shop in style</h1>
+                    <p className="template">With this shop hompeage template</p>
+                </section>
+                <section></section>
+            </main>
+            <footer className='footer'>
+                <p>Copyright © Your Website 2021</p>
+            </footer>
+        </div>
+    );
+}
+
+export default Shoppingpage;
